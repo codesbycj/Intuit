39fbd6be472ec6911c65d98a0eba4d038d6cf735
Developergatsby <gabokekeemmanuel@gmail.com>
2025-05-27 18:46:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const hamburgerActive = document.querySelector(".hamburger");
+const hamburgerUnActive = document.querySelector(".closeburger");
+const navBtn = document.querySelector(".navbar-toggler");
+const variableBurger = document.querySelector(".jsBurger");
+const discount = document.getElementById('discount');
+
+setTimeout(() => {
+    discount.style.display = 'block';
+}, 3000);
+
+console.log(typeof hamburgerActive);
+
+navBtn.addEventListener("click", () => {
+        discount.style.display = 'fixed';
+
+  if (hamburgerActive.classList.contains('jsBurger')) {
+    hamburgerActive.classList.remove("jsBurger");
+    hamburgerActive.classList.add("navUnactive");
+    hamburgerUnActive.classList.remove("navUnactive");
+    console.log(navBtn.innerHTML);
+  } else if (!hamburgerActive.classList.contains('.jsBurger')) {
+    hamburgerActive.classList.add("jsBurger");
+    hamburgerActive.classList.add("hamburger");
+    hamburgerActive.classList.remove("navUnactive");
+    hamburgerUnActive.classList.add("navUnactive");
+    console.log(navBtn.innerHTML);
+  }
+});
